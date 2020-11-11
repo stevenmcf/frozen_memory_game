b81800d9e6079aac061f5d281899f1fc29daeda6
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-11 21:00:03 +0000

    logic added for lockboard, shuffle cards and same card click.

--- scripts.js
+++ scripts.js
@@ -1,43 +1,71 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
+        // first click
         hasFlippedCard = true;
         firstCard = this;
         return;
     }
 
+        // second click
     secondCard = this;
-    hasFlippedCard = false;
+    // lockBoard = true;
+    // hasFlippedCard = false;
 
     checkForMatch();
 }
 
 function checkForMatch() {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        disableCards();
-        return;
-    }
+    // if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    //     disableCards();
+    //     return;
+    // }
 
-    unFlipCards();
+    // unFlipCards();
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unFlipCards();
 }
 
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unFlipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-    }, 1500);
+
+        // lockBoard = false;
+        resetBoard();
+    }, 1000);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+      let randomPos = Math.floor(Math.random() * 12);
+      card.style.order = randomPos;
+    });
+  })();
 
 
 
