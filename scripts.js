a0d965abf9dd4ae5a3b902eb662e21b0ff1ae306
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-11 20:34:12 +0000

    logic added to check for match, disable cards in event of a match and also re-set (unFlipCards()

--- scripts.js
+++ scripts.js
@@ -9,7 +9,36 @@ function flipCard() {
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
 }
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+
+    unFlipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unFlipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
